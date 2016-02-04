36894c4c6df1b209e75d727b48e8fe2d3ccdda14
Axel Smeets <murlocbrand@gmail.com>
2016-02-05 00:07:42 +0100

    lib/bandcamp.js --> index.js and now with less clutter

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+/* Copyright (c) 2016 Axel Smeets */
 
-module.exports = require('./lib/bandcamp.js')
+var qs = require('querystring')
+
+module.exports = exports = function search(query) {
+    var options = {
+        hostname: 'bandcamp.com',
+        path:     '/api/nusearch/2/autocomplete?' + qs.stringify({ q : query }),
+        headers:  {
+            'User-Agent': 'android-async-http/1.4.1 (http://loopj.com/android-async-http)',
+            'Cookie': '$Version=1'
+        }
+    }
+
+    return options
+}
